dee1db9559f6d3b08fa99c7fdcc22482716508ea
Jeremy Langevin <langevinj832@gmail.com>
2020-12-15 07:21:14 -0500

    TodoList component written

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,12 @@
 import React from 'react' 
 
-function Todo({ todo, removeTodo }){
+function Todo({ todo, remove }){
 
+    return (
+        <span>
+
+        </span>
+    )
 
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Todo from './Todo'
 
 function TodoList(){
+    const todos = useSelector(st => st.todos)
+    const dispatch = useDispatch();
 
+    const remove = (evt) => {
+        evt.preventDefault();
+        dispatch({ type: "DELETE", payload: evt.target.id})
+    }
+
+    return (
+        <ul className="Todolist">
+           {todos ? todos.map(todo => <li><Todo todo={todo} remove={remove}/></li>) : null}
+        </ul>
+    )
 }
 
 export default TodoList;
