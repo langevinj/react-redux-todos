7ecada7f48a1ab9b96557b1ed606893b3ce6b1eb
Jeremy Langevin <langevinj832@gmail.com>
2020-12-15 07:13:10 -0500

    corrected typo in rootreducer

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,10 +3,10 @@ const INITIAL_STATE = { todos: [] }
 function rootReducer(state = INITIAL_STATE, action) {
     switch(action.type) {
         case "ADD":
-            return { ...state, todos: [...todos, action.payload]}
+            return { ...state, todos: [...state.todos, action.payload]}
         
         case "DELETE":
-            return { ...state, todos: todos.filter(todo => todo.id !== action.payload.id)}
+            return { ...state, todos: state.todos.filter(todo => todo.id !== action.payload.id)}
 
         default:
             return state;
